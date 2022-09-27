4e7040a4002dda3812490a4993247017723a486e
Bilaal Abdel Hassan <bilaal.abdel.hassan@inpleo.com>
2022-09-27 15:49:18 +0400

    updated script 7

--- nexoka-clear-cache.php
+++ nexoka-clear-cache.php
@@ -1,6 +1,11 @@
 <?php 
 	echo("Before Timer");
 	sleep(240);
-	system("rm -rf var/cache/*");
+	$files = glob('var/cache/*'); // get all file names
+	foreach($files as $file){ // iterate files
+	  if(is_file($file)) {
+		unlink($file); // delete file
+	  }
+	}
 	echo("After Timer");
 ?>
