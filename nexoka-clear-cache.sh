3512f179614f66aa15cc9055771bf7577322da2c
Bilaal Abdel Hassan <bilaal.abdel.hassan@inpleo.com>
2022-09-28 06:32:12 +0400

    updated script 12

--- nexoka-clear-cache.sh
+++ nexoka-clear-cache.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 PHP=`which php`
-echo Before Cache Clear
-ls -la app/
+#echo Before Cache Clear
+echo $PHP
 sleep 240
 $PHP bin/console cache:clear
-echo After Cache Clear
+#echo After Cache Clear
 exit
