e0bdddb1200a2620c76a2bc337947d4ee919a262
Bilaal Abdel Hassan <bilaal.abdel.hassan@inpleo.com>
2022-09-28 13:41:41 +0400

    updated script 19

--- nexoka-clear-cache.sh
+++ nexoka-clear-cache.sh
@@ -4,7 +4,7 @@
 # Check if another instance of this script is running
 pidof -o %PPID -x $0 >/dev/null && echo "ERROR: Script $0 already running" && exit 1
 sleep 240
-#/app/.heroku/php/bin/php bin/console cache:clear
 echo Clearing cache...
-rm -rf var/cache/*
+/app/.heroku/php/bin/php bin/console cache:clear
+#rm -rf var/cache/*
 exit
